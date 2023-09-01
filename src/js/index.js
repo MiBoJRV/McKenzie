7675e7c1d4c812eb9830a9ff48fb48a5d6b71771
Mykhailo Bodnar <mykhailo.bodnar@wetelo.com>
2023-09-01 12:53:36 +0300

    add page content

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,43 @@
 import mobileNav from './modules/mobile-nav.js';
+
 mobileNav();
+
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+
+    keyboard: {
+        enabled: true,
+    },
+
+    mousewheel: true,
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+console.log('nmm')
